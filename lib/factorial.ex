33f14516c83c1097efae20e5b555c47be777ef32
ottoste <ottotahtracing@gmail.com>
2015-11-18 15:24:16 +0000

    calculates factorial and has a guard for negative numbers returning nil

--- lib/factorial.ex
+++ lib/factorial.ex
@@ -1,2 +1,16 @@
 defmodule Factorial do
+	def calculate(0) do
+		1
+	end
+
+	def calculate(n) when n < 0 do
+    nil
+  end
+
+	def calculate(n) do
+		n * calculate(n - 1)
+	end
 end
+
+  
+
--- test/factorial_test.exs
+++ test/factorial_test.exs
@@ -2,7 +2,19 @@ defmodule FactorialTest do
   use ExUnit.Case
   doctest Factorial
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "factorial of 0 is 1" do
+    assert Factorial.calculate(0) == 1
+  end
+
+  test "factorial of 1 is 1" do
+    assert Factorial.calculate(1) == 1
+  end
+
+  test "factorial of 3 is 6" do
+    assert Factorial.calculate(3) == 6
+  end
+
+  test "for negative number return nil" do
+    assert Factorial.calculate(-5) == nil
   end
 end
